aaa108e471545ec58a47a1a994c007d1b25d5072
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:10:54 +0800

    Revised and arrange the line 6

--- multiplication_table.py
+++ multiplication_table.py
@@ -2,13 +2,15 @@
 
 # Pseudocode
 
+# Print starting point 
+print("Multiplcation Table for 1 to 10 ") 
+
 # Create a for loop for multiplicand 
 for multiplicand in range (1, 11):
-# Print "Multiplication Table for " with space
-    print("Multiplcation Table for  ",  multiplicand) 
-    
+
 # Start a nested loop for each number from 1 to 10 as the multiplier
     for multiplier in range(1, 11):
+
         # PRINT multiplicand, "*", multiplier, "=", result
         print(multiplicand * multiplier, end = " ")
     print("\t\t")
