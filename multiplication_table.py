fac5ccf05cff1e13f424faa661a5be821bacb815
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 14:56:45 +0800

    Created for loop for multiplicand

--- multiplication_table.py
+++ multiplication_table.py
@@ -3,7 +3,10 @@
 # Pseudocode
 
 # Create a for loop for multiplicand 
+for number in range (1, 11):
 # Print "Multiplication Table for " with space
+    print("Multiplcation Table for ", number) 
+    
 # Create a for loop for multiplier 
 # Start a nested loop for each number from 1 to 10 as the multiplier
  # PRINT multiplicand, "*", multiplier, "=", result
