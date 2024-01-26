039539ac7d5d6568e477f64fa960b2da0aff0ebb
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 14:53:53 +0800

    Added program statement and created pseudocode

---
+++ multiplication_table.py
@@ -0,0 +1,9 @@
+# Print multiplication table from 1 to 10
+
+# Pseudocode
+
+# Create a for loop for multiplicand 
+# Print "Multiplication Table for " with space
+# Create a for loop for multiplier 
+# Start a nested loop for each number from 1 to 10 as the multiplier
+ # PRINT multiplicand, "*", multiplier, "=", result
